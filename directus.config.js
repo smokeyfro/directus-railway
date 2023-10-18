f6097929a8cdaac035a951ada02af68876d968e4
Chris Rault <chris@smokeyfro.com>
2023-10-18 09:31:06 +0200

    Set the public url to the configured railway url

--- directus.config.js
+++ directus.config.js
@@ -7,12 +7,18 @@ module.exports = function (env) {
         KEY: env.KEY,
         SECRET: env.SECRET,
 
+        STORAGE_LOCATIONS=cloudinary
+        STORAGE_CLOUDINARY_CLOUD_NAME=env.STORAGE_CLOUDINARY_CLOUD_NAME
+        STORAGE_CLOUDINARY_API_KEY=env.STORAGE_CLOUDINARY_API_KEY
+        STORAGE_CLOUDINARY_API_SECRET=env.STORAGE_CLOUDINARY_API_SECRET
+        STORAGE_CLOUDINARY_ACCESS_MODE=authenticated
+        
         // Reference: https://docs.railway.app/deploy/exposing-your-app
         PORT: env.PORT,
 
         // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
-        PUBLIC_URL: `http://0.0.0.0:${env.PORT}`,
-
+        PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
+        
         // Database variables from Railway PostgreSQL Plugin
         // Reference: https://docs.railway.app/plugins/postgresql
         DB_CLIENT: 'pg',
@@ -21,9 +27,18 @@ module.exports = function (env) {
         DB_DATABASE: env.PGDATABASE,
         DB_USER: env.PGUSER,
         DB_PASSWORD: env.PGPASSWORD,
-        API_KEY_PEXELS: env.API_KEY_PEXELS,
-        API_KEY_PIXABAY: env.API_KEY_PIXABAY,
+
         CONTENT_SECURITY_POLICY_DIRECTIVES__IMG_SRC: env.CONTENT_SECURITY_POLICY_DIRECTIVES__IMG_SRC,
-        RIS_DEFAULT_PROVIDER: env.RIS_DEFAULT_PROVIDER
+        
+        // Redis cache
+        RIS_DEFAULT_PROVIDER: env.RIS_DEFAULT_PROVIDER,
+        MESSENGER_STORE: 'redis',
+        SYNCHRONIZATION_STORE: 'redis',
+        REDIS_HOST: env.REDISHOST,
+        REDIS_PORT: env.REDISPORT,
+        CACHE_ENABLED: env.CACHE_ENABLED,
+        CACHE_STORE: 'redis',
+        REDIS_USERNAME: env.REDISUSER,
+        REDIS_PASSWORD: env.REDISPASSWORD,
     };
 };
